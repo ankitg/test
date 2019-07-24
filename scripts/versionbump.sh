04ddaa9ee0c580f5dcb9a57165f25c351470ba09
Ankit Guglani <ankitg@gmail.com>
2019-07-24 10:18:54 -0400

    •   npm version patch --no-commit-hooks;

--- scripts/versionbump.sh
+++ scripts/versionbump.sh
@@ -2,7 +2,7 @@
 currentbranch=$(git rev-parse --abbrev-ref HEAD);
 if [ "$currentbranch" = "master" ]; then 
   echo "Commiting to Master branch, patching version number";
-  npm version patch --no-git-tag-version && git add package.json package-lock.json;
+  npm version patch --no-commit-hooks;
 else 
   echo "Commiting to $currentbranch branch. Not master, version number unchanged";
   currentversion=$(cat package.json | grep '"version":');
