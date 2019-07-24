4ceecdc8b61fc77462edbe49b9f939f07cf701c4
Ankit Guglani <ankitg@gmail.com>
2019-07-24 11:53:34 -0400

    • change 1 + post commit +  --no-git-tag-version

--- scripts/versionbump.sh
+++ scripts/versionbump.sh
@@ -2,7 +2,7 @@
 currentbranch=$(git rev-parse --abbrev-ref HEAD);
 if [ "$currentbranch" = "master" ]; then 
   echo "Commiting to Master branch, patching version number";
-  npm version --no-commit-hooks patch -m "Version bumped to %s";
+  npm version --no-commit-hooks  --no-git-tag-version patch -m "Version bumped to %s";
 else 
   echo "Commiting to $currentbranch branch. Not master, version number unchanged";
   currentversion=$(cat package.json | grep '"version":');
