202cde7fcbb037430adcccd436f682ab5db1c0a6
Ankit Guglani <ankitg@gmail.com>
2019-07-24 12:40:56 -0400

    • test

--- scripts/versionbump.sh
+++ scripts/versionbump.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 currentbranch=$(git rev-parse --abbrev-ref HEAD);
-if [ "$currentbranch" = "master" ]; then 
-  echo "Commiting to Master branch, patching version number";
-  npm version --no-commit-hooks  --no-git-tag-version patch -m "Version bumped to %s";
+if [ "$currentbranch" = "master" ]; then
+  lastcommitmsg=$(git log -1 --pretty=%B);
+  echo "Last commit was: $lastcommitmsg";
+  if [[ ! "$lastcommitmsg" == "[bump]"* ]]; then
+    echo "Commiting to Master branch, patching version number";
+    npm version --no-commit-hooks  --no-git-tag-version patch -m "[bump] Version bumped to %s";
+  else
+    echo "Not bumping version on master. Last commit was $lastcommitmsg";
+  fi
 else 
   echo "Commiting to $currentbranch branch. Not master, version number unchanged";
   currentversion=$(cat package.json | grep '"version":');
   echo "$currentversion";
 fi
-
