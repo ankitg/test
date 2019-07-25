ed84f7dfb6a3ad31d40eae90158f3c8325a21f3a
Ankit Guglani <ankitg@gmail.com>
2019-07-25 01:07:22 -0400

    • postversion lifecycle event-hook

--- scripts/versionbump.sh
+++ scripts/versionbump.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "Welcome to Version BUMP";
+
+if [ "$1" = "BUMP-PUSH-TAGS" ]; then
+  git push origin master --follow-tags &&
+  exit 0;
+fi
+
 currentbranch=$(git rev-parse --abbrev-ref HEAD);
 if [ "$currentbranch" = "master" ]; then
   lastcommitmsg=$(git log -1 --pretty=%B);
@@ -7,7 +13,6 @@ if [ "$currentbranch" = "master" ]; then
   if [[ ! "$lastcommitmsg" == "[bump]"* ]]; then
     echo "Commiting to Master branch, patching version number";
     npm version patch -m "[bump] Version bumped to %s";
-    git push —-tags origin master
   else
     echo "Not bumping version on master. Last commit was $lastcommitmsg";
   fi
@@ -16,3 +21,4 @@ else
   currentversion=$(cat package.json | grep '"version":');
   echo "$currentversion";
 fi
+exit 0;
